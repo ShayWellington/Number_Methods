1ab34c795f52b150d64f810d22c99e218ed8fa22
ShayWellington <53454314+ShayWellington@users.noreply.github.com>
2019-10-16 00:12:55 +1300

    Add files via upload

---
+++ number_methods/js/script.js
@@ -0,0 +1,19 @@
+// Max Value
+
+var x = Number.MAX_VALUE;
+document.getElementById("result1").innerHTML = x;
+
+// Min Value
+
+var x = Number.MIN_VALUE;
+document.getElementById("result2").innerHTML = x;
+
+// Positive Infinity
+
+var x = Number.POSITIVE_INFINITY;
+document.getElementById("result3").innerHTML = x;
+
+// Negative Infinity
+
+var x = Number.NEGATIVE_INFINITY;
+document.getElementById("result4").innerHTML = x;
